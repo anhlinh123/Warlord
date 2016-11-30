6c3be18009efe7e57451b1505e6a64d462fac68f
Anh Linh Nguyen <linh.lrd@hotmail.com>
2016-12-01 01:02:52 +0700

    + add main project + add premake script

---
+++ proj/premake.bat
@@ -0,0 +1,5 @@
+@echo off
+pushd %~dp0\..\lib\glfw
+git clean -ffxd
+cmake -G "Visual Studio 12 2013" -DBUILD_SHARED_LIBS=OFF -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_INSTALL=OFF
+popd
